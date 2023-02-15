7cc74ce18a230fe9d3a8fff2f6faf75f005e3522
Kago <kmotlhabane769@gmail.com>
2023-02-15 13:37:36 +0200

    Color based on event listener on window

--- script.js
+++ script.js
@@ -41,6 +41,44 @@ function populateGrid(numberOfDivs) {
     body.appendChild(grid);
 }
 
+function getDrawModeOption(){
+
+}
+
 // let numberOfDivs = getNumberOfDivs();
 let numberOfDivs = 16;
 populateGrid(numberOfDivs);
+
+let isMouseDownTriggered = false;
+
+// Code for if hover and drag mode is selected
+window.addEventListener('mousedown', function(){
+    isMouseDownTriggered = true;
+});
+
+window.addEventListener('mouseup', function(){
+    isMouseDownTriggered = false;
+});
+
+window.addEventListener('mouseover', function (e) {
+    console.log(e.target);
+    if (isMouseDownTriggered === true) {
+        e.target.style.backgroundColor = "white";
+    }
+});
+
+/* Unbind event listener from window. There's an issue that onMouseOver it reads as false until its clicked which is a good thing, of course it won't color
+until both are satisfied but it will jump into the debugger each time which is really annoying until that condition is satisfied but I can't
+satisfy it since debugger is irritating. On hover it immediately takes me there without giving me a chance to click. So only place the flag
+within the inner part where the if statement is satisfied. */
+/*We don't even need to unbind from window. That's not the problem. We can make sure the color in code executes only when the right div is selected too.*/
+/* Do this by checking the targets properties. Not sure if it was covered, clicking a child element clicks both the child and the parent.
+I believe that was covered. So it does affect the events. Not just the parent is clicked, but the child too. And if they both had the same
+click event then that would be a problem. We don't have that here though.*/
+/* We also never have a break on mouseover. It is constantly on mouseover. */
+
+// let drawMode = getDrawModeOption();
+/* Setup event listener on field for when text is entered, then enter into a different draw mode each time. 
+This allows code to be implemented anytime regardless of the flow of the program. The flow needs to be dynamic not linear anyway. */
+
+
