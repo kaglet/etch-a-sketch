145aeba2f8d381bc630d4011aea2695b24e65d90
Kago <kmotlhabane769@gmail.com>
2023-12-11 22:11:31 +0200

    Remove default appearance of color picker

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 // GLOBAL VARIABLES
-
 let gridLinesPresent = false;
 let gridWidth = 500;
 
 // FUNCTIONS
-
 function removeGridEventListeners(grid) {
     // Remove all grid event listeners that might be active
     grid.removeEventListener('mousedown', grid.fnMouseDown);
@@ -101,47 +99,6 @@ function getGridBackgroundColor() {
 
 function rgbToHex(r, g, b) {
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-function getDarkerColor(col, amt) {
-    let r = col.substring(4, col.indexOf(","));
-    col = col.slice(col.indexOf(",") + 1);
-    let g = col.substring(1, col.indexOf(","));
-    col = col.slice(col.indexOf(",") + 1);
-    let b = col.substring(1, col.indexOf(")"));
-    col = rgbToHex(+r, +g, +b);
-
-    let usePound = false;
-
-    if (col[0] == "#") {
-        col = col.slice(1);
-        usePound = true;
-    }
-
-    let R = parseInt(col.substring(0, 2), 16);
-    let G = parseInt(col.substring(2, 4), 16);
-    let B = parseInt(col.substring(4, 6), 16);
-
-    // to make the colour less bright than the input
-    // change the following three "+" symbols to "-"
-    R = R - amt;
-    G = G - amt;
-    B = B - amt;
-
-    if (R > 255) R = 255;
-    else if (R < 0) R = 0;
-
-    if (G > 255) G = 255;
-    else if (G < 0) G = 0;
-
-    if (B > 255) B = 255;
-    else if (B < 0) B = 0;
-
-    var RR = ((R.toString(16).length == 1) ? "0" + R.toString(16) : R.toString(16));
-    var GG = ((G.toString(16).length == 1) ? "0" + G.toString(16) : G.toString(16));
-    var BB = ((B.toString(16).length == 1) ? "0" + B.toString(16) : B.toString(16));
-
-    return (usePound ? "#" : "") + RR + GG + BB;
 }
 
 function colorAccordingToPenMode(e) {
